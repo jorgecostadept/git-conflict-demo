de7ee06c2388e976864d80737ea9cc926cbe1876
Jorge Costa <jorge.costa@deptagency.com>
2025-04-09 14:00:28 +0200

    update example 2

--- DemoService.java
+++ DemoService.java
@@ -2,7 +2,7 @@ public class DemoService {
 
     public String getName() {
         System.out.println("Getting name");
-        return "BaseName";
+        return "Feature1";
     }
 
     public int calculateScore(int a, int b) {
@@ -11,6 +11,21 @@ public class DemoService {
         return result;
     }
 
+    //Duplicate code with random names
+
+    public String getName1() {
+        System.out.println("Getting name1");
+        return "Feature2";
+    }
+
+    public int calculateScore1(int a, int b) {
+        int result = a + b;
+        System.out.println("Score1: " + result);
+        return result;
+    }
+
+    //end of duplicate code
+    
     public boolean isActive() {
         return true;
     }
