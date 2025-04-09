25f7ed16c9395628d1ad4dd5f216f676c74cd064
Jorge Costa <jorge.costa@deptagency.com>
2025-04-09 11:23:00 +0200

    Add logging to JS functions

--- calculator.js
+++ calculator.js
@@ -1,1 +1,14 @@
-function add(a, b) { return a + b; }
+function add(a, b) {
+    console.log("Adding numbers");
+    return a + b;
+}
+
+function multiply(a, b) {
+    console.log("Multiplying");
+    return a * b;
+}
+
+function divide(a, b) {
+    console.log("Dividing");
+    return b === 0 ? 0 : a / b;
+}
