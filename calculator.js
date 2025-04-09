8521a6decda301b378dc2e3a365f77833d31dc6f
Jorge Costa <jorge.costa@deptagency.com>
2025-04-09 11:22:39 +0200

    Change add to subtract and add complex JS functions

--- calculator.js
+++ calculator.js
@@ -1,1 +1,12 @@
-function add(a, b) { return a + b; }
+function add(a, b) {
+    return a - b; // intentional change
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    if (b === 0) return 0;
+    return a / b;
+}
