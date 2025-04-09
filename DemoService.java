030b93ae1caa6a87dd4ec7f73f395502622c91a2
Jorge Costa <jorge.costa@deptagency.com>
2025-04-09 14:06:18 +0200

    add base complex file

---
+++ DemoService.java
@@ -0,0 +1,16 @@
+public class DemoService {
+    //Duplicate code with random names
+
+    public String getName1() {
+        System.out.println("Getting name1");
+        return "Feature2";
+    }
+
+    public int calculateScore1(int a, int b) {
+        int result = a + b;
+        System.out.println("Score1: " + result);
+        return result;
+    }
+
+    //end of duplicate code
+}
