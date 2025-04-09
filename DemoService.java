e74da5ee015def83d9f72593596a1e03aff1f170
Jorge Costa <jorge.costa@deptagency.com>
2025-04-09 13:58:52 +0200

    update code

--- DemoService.java
+++ DemoService.java
@@ -4,15 +4,31 @@ public class DemoService {
         return "Feature1";
     }
 
-    public int calculateScore(int a, int b) {
-        return a - b; // changed logic
-    }
-
     public boolean isActive() {
         return false; // logic flipped
     }
 
+    //Duplicate code with random names
+
+    public String getName1() {
+        System.out.println("Getting name1");
+        return "Feature2";
+    }
+
+    public int calculateScore1(int a, int b) {
+        int result = a + b;
+        System.out.println("Score1: " + result);
+        return result;
+    }
+
+    //end of duplicate code
+
     public void logAccess() {
         System.out.println("Accessed");
+    }
+
+    public int calculateScore(int a, int b) {
+       int result = a + b;
+       return result;
     }
 }
