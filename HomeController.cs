76d5243acf92368b1d47c0d8b7a6ae1e86f23dba
Jorge Costa <jorge.costa@deptagency.com>
2025-04-09 11:23:34 +0200

    Add logging to controller methods

--- HomeController.cs
+++ HomeController.cs
@@ -1,1 +1,9 @@
-public string GetWelcomeMessage() { return "Welcome to the app!"; }
+public string GetWelcomeMessage() {
+    Console.WriteLine("Welcome message sent");
+    return "Welcome to the app!";
+}
+
+public string GetGoodbyeMessage() {
+    Console.WriteLine("Goodbye message sent");
+    return "Goodbye!";
+}
