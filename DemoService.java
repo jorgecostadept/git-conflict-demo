e6e3e8688ee8770ea56ba9c1e0c480fcbeaf8e9e
Jorge Costa <jorge.costa@deptagency.com>
2025-04-09 11:25:25 +0200

    Feature1 edits in DemoService

--- DemoService.java
+++ DemoService.java
@@ -1,1 +1,18 @@
-public class DemoService {\n\n    public String getName() {\n        return "BaseName";\n    }\n\n    public int calculateScore(int a, int b) {\n        return a + b;\n    }\n\n    public boolean isActive() {\n        return true;\n    }\n\n}
+public class DemoService {
+
+    public String getName() {
+        return "Feature1";
+    }
+
+    public int calculateScore(int a, int b) {
+        return a - b; // changed logic
+    }
+
+    public boolean isActive() {
+        return false; // logic flipped
+    }
+
+    public void logAccess() {
+        System.out.println("Accessed");
+    }
+}
